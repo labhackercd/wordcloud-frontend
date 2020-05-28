c0488c10aa3c2154271f2f0fda3609337a92fbd0
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2020-05-28 15:19:41 -0300

    Adding new folders and creating initial page

--- wordcloud-front/src/App.js
+++ wordcloud-front/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { COLORS } from "./config/config_constants";
+import "./App.css";
+
+import InitialPage from "./containers/InitialPage"; 
+
+import { ptBR } from "@material-ui/core/locale";
+
+const theme = createMuiTheme(
+  {
+    palette: {
+      primary: {
+        main: COLORS.mainColor
+      }
+    }
+  },
+  ptBR
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <InitialPage></InitialPage>
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ wordcloud-front/src/config/config_constants.js
@@ -0,0 +1,18 @@
+
+module.exports = Object.freeze({ 
+  // SYSTEM SETTING
+  PORT_IN_USE: ":8000",
+
+  //PORT_IN_USE_SERVER:'', //USE THIS IN CASE OF PRODUCTION SERVER
+  IS_LOCAL_TEST_ENV: true,
+
+  // URL'S
+  APPLICATION_LOCAL_URL: "http://localhost:8000",
+
+  COLORS : {
+    mainColor: '#28a745',
+    mainBackgroundColor: '#ffffff',
+    menuBackgroundColor: '#e1e5f3'
+  }
+
+});
